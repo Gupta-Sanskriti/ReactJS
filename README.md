fe8575ebf467a21d0985d9a0c64eb41b953f7614
Sanskriti Gupta <21sanskritigupta@gmail.com>
2023-01-04 22:03:48 +0530

    added old way of creating element by createElement

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ReactJS
+
 Namaste react JS
+Understanding of react and it's core concepts which includes :
+1. Babel, 
+2. JSX, 
+3. Parcel, 
+4. creating React app
+5. functional component
---
+++ oldapp.js
@@ -0,0 +1,11 @@
+// Old way of creating the react app without using parcel or babel or JSX
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const heading = React.createElement("h1", {id:"heading1"}, "Hello this is the traditional way of creating react app");
+const paragraph = React.createElement("p", {id: "para1", className:"newPara"}, "Hello this is a paragraph")
+
+const container = React.createElement("div", {id:"container"},[heading, paragraph]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container)
